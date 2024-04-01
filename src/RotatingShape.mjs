c37c78498f6138340e05a064cf594abaee492e52
hafezfhmi <hafezfahmi2000@gmail.com>
2024-04-02 05:29:52 +0800

    feat: implement shape left rotation

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -21,6 +21,15 @@ export class RotatingShape {
     return new RotatingShape(rotatedShape);
   }
 
+  rotateLeft() {
+    let rotatedShape = [];
+    for (let col = this.shape[0].length - 1; col >= 0; col--) {
+      let str = "";
+      for (let row = 0; row < this.shape.length; row++) str += this.shape[row][col];
+      rotatedShape.push(str);
+    }
+    return new RotatingShape(rotatedShape);
+  }
   toString() {
     return this.shape.join("\n") + "\n";
   }
