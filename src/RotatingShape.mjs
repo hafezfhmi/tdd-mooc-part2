0bfe2faa5abb390d7d88ff3067aaff0afd7a1235
hafezfhmi <hafezfahmi2000@gmail.com>
2024-04-02 05:08:55 +0800

    refactor: implement proper algorithm for shapre rotate right

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -10,7 +10,15 @@ export class RotatingShape {
   }
 
   rotateRight() {
-    return new RotatingShape(["GDA", "HEB", "IFC"]);
+    let rotatedShape = [];
+    for (let col = 0; col < this.shape.length; col++) {
+      let str = "";
+      for (let row = this.shape[col].length - 1; row >= 0; row--) {
+        str += this.shape[row][col];
+      }
+      rotatedShape.push(str);
+    }
+    return new RotatingShape(rotatedShape);
   }
 
   toString() {
