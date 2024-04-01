93e8152b94465fbe8003fd113bf7abfcb27886e4
hafezfhmi <hafezfahmi2000@gmail.com>
2024-04-01 20:39:04 +0800

    feat: create move block one level per tick functionality

--- src/Board.mjs
+++ src/Board.mjs
@@ -2,14 +2,20 @@ export class Board {
   width;
   height;
   block;
+  dropLevel;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.dropLevel = 0;
   }
 
   drop(block) {
     this.block = block;
+  }
+
+  tick() {
+    this.dropLevel++;
   }
 
   toString() {
@@ -17,7 +23,7 @@ export class Board {
 
     for (let row = 0; row < this.height; row++) {
       for (let col = 0; col < this.width; col++) {
-        if (this.block && row === 0 && col === 1) {
+        if (this.block && row === this.dropLevel && col === 1) {
           result += this.block;
         } else {
           result += ".";
