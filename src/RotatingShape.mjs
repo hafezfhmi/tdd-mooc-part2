8b7f3ad5e92923068acfb72acdbf1884e67886aa
hafezfhmi <hafezfahmi2000@gmail.com>
2024-04-02 05:42:35 +0800

    refactor: improve code readability

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -25,11 +25,14 @@ export class RotatingShape {
     let rotatedShape = [];
     for (let col = this.shape[0].length - 1; col >= 0; col--) {
       let str = "";
-      for (let row = 0; row < this.shape.length; row++) str += this.shape[row][col];
+      for (let row = 0; row < this.shape.length; row++) {
+        str += this.shape[row][col];
+      }
       rotatedShape.push(str);
     }
     return new RotatingShape(rotatedShape);
   }
+
   toString() {
     return this.shape.join("\n") + "\n";
   }
