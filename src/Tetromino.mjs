e881bd5c4acc6eb404f8badca6c86b78f27f0721
hafezfhmi <hafezfahmi2000@gmail.com>
2024-04-04 12:44:24 +0800

    refactor: use currentOrientations and orientations in Tetromino toString metho

--- src/Tetromino.mjs
+++ src/Tetromino.mjs
@@ -40,7 +40,7 @@ export class Tetromino {
   }
 
   toString() {
-    return this.shape.toString();
+    return this.orientations[this.currentOrientation].toString();
     // 4. use currentOrientation and orientations to get current shape.
   }
 
